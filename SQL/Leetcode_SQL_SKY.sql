99c43901fceb8e22ac491809e02402701df0c67d
SKYoooon <skyoon7899@gmail.com>
2024-02-20 21:09:42 +0900

    0220

--- SQL/Leetcode_SQL_SKY.sql
+++ SQL/Leetcode_SQL_SKY.sql
@@ -69,6 +69,14 @@ WITH PreviousWeatherData AS
     FROM 
         Weather
 )
+SELECT 
+    id 
+FROM 
+    PreviousWeatherData
+WHERE 
+    temperature > PreviousTemperature
+AND 
+    recordDate = DATE_ADD(PreviousRecordDate, INTERVAL 1 DAY);
 -- lag
 
 
