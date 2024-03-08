af6e010811ca9f9fbefbf3ceeb2895a5db7a4b32
SKYoooon <skyoon7899@gmail.com>
2024-03-08 23:08:49 +0900

    0308

--- SQL/Programmers_SQL_SKY.sql
+++ SQL/Programmers_SQL_SKY.sql
@@ -1,18 +1,73 @@
---12세 이하인 여자 환자 목록 출력하기
+--재구매가 일어난 상품과 회원 리스트 구하기
+
+
+
+--********
+--********
+
+
+--강원도에 위치한 생산공장 목록 출력하기
 SELECT
-    PT_NAME,
-    PT_NO,
-    GEND_CD,
-    AGE,
-    IFNULL(TLNO, 'NONE') TLNO
+    factory_id,
+    factory_name,
+    address
 FROM
-    patient
+    food_factory
 WHERE
-    AGE <=12
-    AND GEND_CD = "W"
+    address like '강원도%'
+
+
+--********
+--********
+
+
+--과일로 만든 아이스크림 고르기/ 1. JOIN 2. USING
+SELECT
+    F.FLAVOR
+FROM
+    FIRST_HALF F
+JOIN
+    ICECREAM_INFO I
+ON
+    F.FLAVOR = I.FLAVOR
+WHERE
+    F.TOTAL_ORDER > 3000
+    AND I.INGREDIENT_TYPE = 'fruit_based'
 ORDER BY
-    AGE DESC,
-    PT_NAME ASC;
+    F.FLAVOR DESC;
+
+--------
+
+SELECT
+    FLAVOR
+FROM
+    FIRST_HALF
+JOIN
+    ICECREAM_INFO USING(FLAVOR)
+WHERE
+    TOTAL_ORDER>3000
+AND INGREDIENT_TYPE = 'fruit_based'
+ORDER BY
+    TOTAL_ORDER DESC;
+
+
+--********
+--********
+
+
+--인기있는 아이스크림
+SELECT
+    FLAVOR
+FROM
+    FIRST_HALF
+ORDER BY
+    TOTAL_ORDER DESC,
+    SHIPMENT_ID ASC
+    
+
+--********
+--********
+
 
 --흉부외과 또는 일반외과 의사 목록 출력하기
 SELECT
@@ -28,42 +83,23 @@ ORDER BY
     HIRE_YMD DESC,
     DR_NAME ASC
 
---인기있는 아이스크림
-SELECT
-    FLAVOR
-FROM
-    FIRST_HALF
-ORDER BY
-    TOTAL_ORDER DESC,
-    SHIPMENT_ID ASC
+
+--********
+--********
 
 
---과일로 만든 아이스크림 고르기
+--12세 이하인 여자 환자 목록 출력하기
 SELECT
-    F.FLAVOR
+    PT_NAME,
+    PT_NO,
+    GEND_CD,
+    AGE,
+    IFNULL(TLNO, 'NONE') TLNO
 FROM
-    FIRST_HALF F
-JOIN
-    ICECREAM_INFO I
-ON
-    F.FLAVOR = I.FLAVOR
+    patient
 WHERE
-    F.TOTAL_ORDER > 3000
-    AND I.INGREDIENT_TYPE = 'fruit_based'
+    AGE <=12
+    AND GEND_CD = "W"
 ORDER BY
-    F.FLAVOR DESC;
---JOIN
-
-SELECT
-    FLAVOR
-FROM
-    FIRST_HALF
-JOIN
-    ICECREAM_INFO USING(FLAVOR)
-WHERE
-    TOTAL_ORDER>3000
-AND INGREDIENT_TYPE = 'fruit_based'
-ORDER BY
-    TOTAL_ORDER DESC;
---USING
-
+    AGE DESC,
+    PT_NAME ASC;
